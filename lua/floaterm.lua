a398561cc6cb4d5d5c30bfee9aa8179de2280a34
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2025-08-09 17:47:57 -0700

    Basic window toggle functionality

--- lua/floaterm.lua
+++ lua/floaterm.lua
@@ -1,1 +1,31 @@
-print("floaterm.nvim")
+local M = {
+    buffer = nil,
+    window = nil,
+}
+
+function M.toggle_terminal()
+    if M.buffer == nil then
+        M.buffer = vim.api.nvim_create_buf(false, true)
+    end
+
+    if M.window == nil or not vim.api.nvim_win_is_valid(M.window) then
+        opts =  {
+            relative="editor",
+            anchor="NW",
+            width=50,
+            height=50,
+            row=20,
+            col=20,
+            style="minimal"
+        }
+        M.window = vim.api.nvim_open_win(M.buffer, true, opts)
+    else
+        vim.api.nvim_win_hide(M.window)
+    end
+end
+
+function M.setup()
+    vim.keymap.set({"n", "t"}, "<leader>tt", M.toggle_terminal)
+end
+
+return M
